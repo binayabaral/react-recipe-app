d60dbe1fa82ebb356187612c52aabba76911a3f1
Binaya Baral <binaya.baral5@gmail.com>
2020-06-28 21:41:55 +0545

    v1

---
+++ build/precache-manifest.ac26e8774e2957529b68c0ad53a7a374.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d77c917b2c3dfa537584e4f233f1190",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b61fce3dce916ce8bedf",
+    "url": "/static/css/main.b432c4f8.chunk.css"
+  },
+  {
+    "revision": "fbe1a8c0493d2eb0c75d",
+    "url": "/static/js/2.108b7a93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.108b7a93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b61fce3dce916ce8bedf",
+    "url": "/static/js/main.bd7f7224.chunk.js"
+  },
+  {
+    "revision": "3288c5be6d20e1d4b566",
+    "url": "/static/js/runtime-main.97febd3c.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Recipe from './Recipe';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const APP_ID = '12a11a58';
+	const API_KEY = 'a46c0717b750ad84c8fc178854ed0866';
+
+	// const example_req = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY}`;
+
+	const [recipes, setRecipes] = useState([]);
+	const [search, setSearch] = useState('');
+	const [query, setQuery] = useState('chicken');
+
+	useEffect(() => {
+		getRecipes();
+	}, [query]);
+
+	const getRecipes = async () => {
+		const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${API_KEY}`);
+		const data = await response.json();
+		setRecipes(data.hits);
+		console.log(data.hits);
+	};
+
+	const updateSearch = e => {
+		setSearch(e.target.value);
+	};
+
+	const getSearch = e => {
+		e.preventDefault();
+		setQuery(search);
+		setSearch('');
+	};
+
+	return (
+		<div className="App">
+			<form className="search-form" onSubmit={getSearch}>
+				<input className="search-bar" type="text" value={search} onChange={updateSearch} />
+				<button type="submit" className="search-button">
+					Search
+				</button>
+			</form>
+			<div className="recipes">
+				{recipes.map(recipe => (
+					<Recipe key={recipe.recipe.label} title={recipe.recipe.label} calories={recipe.recipe.calories} image={recipe.recipe.image} ingredients={recipe.recipe.ingredients} />
+				))}
+			</div>
+		</div>
+	);
+};
 
 export default App;
